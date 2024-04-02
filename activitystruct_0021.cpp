f642d79048e77597d1949186b1649d0b65b5c3c2
PannnTastic <pannntastic@gmail.com>
2024-04-02 18:48:15 +0800

    input pengguna

--- activitystruct_0021.cpp
+++ activitystruct_0021.cpp
@@ -6,9 +6,19 @@ struct mahasiswa
     string nim;
     string nama;
     string alamat;
-    string umur;
+    int umur;
 };
 
 int main (){
     mahasiswa mhs;
+
+    cout<< "Nomor Mahasiswa : ";
+    cin >> mhs.nim;
+    cout<< "Nama Mahasiswa : ";
+    cin >> mhs.nama;
+    cout << "Alamat Mahasiswa : ";
+    cin >> mhs.alamat;
+    cout << "Umur : ";
+    cin >> mhs.umur;
+
 }
